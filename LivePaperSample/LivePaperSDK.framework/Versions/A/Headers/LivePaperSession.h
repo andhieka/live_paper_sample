19455bf8991bb7d50c4023208829cbd00ca619fb
Soraya Sherwin and Steven Say <soraya.sherwin+steven.say@hp.com>
2015-02-08 13:22:42 -0800

    Update sample to show creating watermark with rich payoff data.

--- LivePaperSample/LivePaperSDK.framework/Versions/A/Headers/LivePaperSession.h
+++ LivePaperSample/LivePaperSDK.framework/Versions/A/Headers/LivePaperSession.h
@@ -13,6 +13,10 @@
 
 + (instancetype)createSessionWithClientID:(NSString *) clientID secret:(NSString *) secret;
 
+@property (readonly, retain) NSString *accessToken;
+
+- (void) retrieveAccessToken:(void (^)(NSString *accessToken, NSError *error)) handler;
+
 - (void) createShortUrl:(NSString *) name destination:(NSURL *) url completionHandler:(void (^)(NSURL *shortUrl, NSError *error)) handler;
 
 - (void) createQrCode:(NSString *) name destination:(NSURL *) url completionHandler:(void (^)(UIImage *qrCodeImage, NSError *error)) handler;
@@ -20,5 +24,9 @@
 - (void) createWatermark:(NSString *) name destination:(NSURL *) url image:(UIImage*) image completionHandler:(void (^)(UIImage *watermarkedImage, NSError *error)) handler;
 
 - (void) createWatermark:(NSString *) name destination:(NSURL *) url imageURL:(NSURL *) imageURL completionHandler:(void (^)(UIImage *watermarkedImage, NSError *error)) handler;
+
+- (void) createWatermark:(NSString *) name richPayoffData:(NSDictionary *) richPayoffData publicURL:(NSURL *) publicURL image:(UIImage *) image completionHandler:(void (^)(UIImage *watermarkedImage, NSError *error)) handler;
+
+- (void) createWatermark:(NSString *) name richPayoffData:(NSDictionary *) richPayoffData publicURL:(NSURL *) publicURL imageURL:(NSURL *) imageURL completionHandler:(void (^)(UIImage *watermarkedImage, NSError *error)) handler;
 
 @end
